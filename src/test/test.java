04ede626e32bf593d1efe7fe8277f3e16052adf9
Pablo17p <pablopr.95@hotmail.com>
2016-10-26 16:45:34 +0200

    alCuadrado

---
+++ src/test/test.java
@@ -0,0 +1,14 @@
+package test;
+
+public class test {
+	
+	public static void main(String[] args) {
+		System.out.println(alCuadrado(3));
+
+	}
+	
+	public static Integer alCuadrado(int a){
+		return a^2;
+	}
+	
+}
